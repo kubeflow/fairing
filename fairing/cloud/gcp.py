0dfae8eed762dae1db389faa4338845da352d00b
Rajiv Pasricha <rpasricha@users.noreply.github.com>
2019-03-07 07:55:01 -0800

    Check additional locations when retrieving project_id and credentials (#103)
    * Update guess_project_name to check more credential options
    
    * Add guess_project_name tests
    
    * Upgrade pytest in travis config

--- fairing/cloud/gcp.py
+++ fairing/cloud/gcp.py
@@ -1,3 +1,4 @@
+import google.auth
 from google.cloud import storage
 from fairing.constants import constants
 
@@ -22,13 +23,23 @@ class GCSUploader(object):
         return "gs://{}/{}".format(bucket_name, blob_name)
 
 
-def guess_project_name(
-        credentials_file=os.environ.get(constants.GOOGLE_CREDS_ENV)):
-    if credentials_file is None:
-        raise Exception("""No credential file provided.
-        Please set GOOGLE_APPLICATION_CREDENTIALS environment variable to point
-        to a credentials file.""")
-    with open(credentials_file, 'r') as f:
-        data = f.read()
-    parsed_creds_file = json.loads(data)
-    return parsed_creds_file['project_id']
+def guess_project_name(credentials_file=None):
+    # Check credentials file if provided
+    if credentials_file is not None:
+        with open(credentials_file, 'r') as f:
+            data = f.read()
+        parsed_creds_file = json.loads(data)
+        return parsed_creds_file['project_id']
+
+    # Use the google.auth library to retrieve credentials. Checks the following
+    # locations in order:
+    # 1. GOOGLE_APPLICATION_CREDENTIALS environment variable
+    # 2. Google Cloud SDK (gcloud)
+    # 3. App Engine Identity service
+    # 4. Compute Engine Metadata service
+    credentials, project_id = google.auth.default()
+
+    if project_id is None:
+        raise Exception('Could not determine project id.')
+
+    return project_id
