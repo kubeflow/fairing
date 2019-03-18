79179cc4227bc15011ad62e8a5ff94a3055a3e06
Karthik Ramasamy <karthikv2k@gmail.com>
2019-03-17 19:35:13 -0700

    Added support for creating endpoint using function preprocessor
    1. Made function preprocessor compatible with prediction deployment jobs
    2. Corrected a typo in TFJob deployer
    3. Created an example for train and deploy of xgboost in gcp

--- fairing/preprocessors/function.py
+++ fairing/preprocessors/function.py
@@ -2,14 +2,21 @@ import cloudpickle
 import fairing
 import glob
 import os
+import types
 import tempfile
 from pathlib import Path
+from enum import Enum
 
 from fairing.constants import constants
 from .base import BasePreProcessor
+from fairing.functions.function_shim import get_execution_obj_type, ObjectType
 
 FUNCTION_SHIM = 'function_shim.py'
 SERIALIZED_FN_FILE = 'pickled_fn.p'
+
+OUPUT_FILE = """import cloudpickle
+{OBJ_NAME} = cloudpickle.load(open("{SERIALIZED_FN_FILE}", "rb"))
+"""
 
 class FunctionPreProcessor(BasePreProcessor):
     """
@@ -26,6 +33,9 @@ class FunctionPreProcessor(BasePreProcessor):
             output_map=output_map,
             path_prefix=path_prefix)
 
+        if get_execution_obj_type(function_obj) ==  ObjectType.NOT_SUPPORTED:
+            raise RuntimeError("Object must of type function or a class")
+        
         fairing_dir = os.path.dirname(fairing.__file__)
         self.output_map[os.path.join(fairing_dir, "functions", FUNCTION_SHIM)] = \
             os.path.join(path_prefix, FUNCTION_SHIM)
@@ -51,6 +61,14 @@ class FunctionPreProcessor(BasePreProcessor):
         # Adding the serialized file to the context
         payload_file_in_context = os.path.join(path_prefix, SERIALIZED_FN_FILE)
         self.output_map[temp_payload_file] = payload_file_in_context
+
+        _, temp_payload_wrraper_file = tempfile.mkstemp()            
+        with open(temp_payload_wrraper_file, "w") as f:
+            contents  = OUPUT_FILE.format(OBJ_NAME=function_obj.__name__, SERIALIZED_FN_FILE=SERIALIZED_FN_FILE)
+            f.write(contents)
+        # Adding the serialized file to the context
+        payload_wrapper_file_in_context = os.path.join(path_prefix, function_obj.__name__ + ".py")
+        self.output_map[temp_payload_wrraper_file] = payload_wrapper_file_in_context
 
         self.command = ["python", os.path.join(self.path_prefix, FUNCTION_SHIM),
                         "--serialized_fn_file", payload_file_in_context]
