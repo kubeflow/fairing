d4b76be0a503d76a108a53644156f7e57d95a125
Rajiv Pasricha <rpasricha@users.noreply.github.com>
2019-03-27 17:24:04 -0700

    Full notebook preprocessor updates (#142)
    * Add input_files to full_notebook preprocessor and update jupyter command
    
    * Normalize input paths in base preprocessor
    
    * Add todo for configuration and check if notebook file has been provided

--- fairing/preprocessors/full_notebook.py
+++ fairing/preprocessors/full_notebook.py
@@ -5,18 +5,25 @@ from fairing.notebook import notebook_util
 
 
 class FullNotebookPreProcessor(BasePreProcessor):
+    # TODO: Allow configuration of errors / timeout options
     def __init__(self,
                  notebook_file=None,
-                 command=["jupyter", "nbconvert", "--stdout", "--to", "notebook", "--execute"],
+                 input_files=None,
+                 command=["jupyter", "nbconvert", "--stdout", "--to", "notebook", "--execute", "--allow-errors",
+                          "--ExecutePreprocessor.timeout=-1"],
                  path_prefix=constants.DEFAULT_DEST_PREFIX,
                  output_map=None):
-        
+
         if notebook_file is None and notebook_util.is_in_notebook():
             notebook_file = notebook_util.get_notebook_name()
-        
+
+        input_files = input_files or []
+        if notebook_file not in input_files:
+            input_files.append(notebook_file)
+
         super().__init__(
             executable=notebook_file,
-            input_files=[notebook_file],
+            input_files=input_files,
             command=command,
             output_map=output_map,
             path_prefix=path_prefix)
