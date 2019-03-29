deb0a5727c6490cbc2f296390c5e1b8347bc71e8
Karthik Ramasamy <karthikv2k@gmail.com>
2019-03-29 11:22:20 -0700

    Added tests and refactored some code to make testing easier

--- tests/unit/builders/test_dockerfile.py
+++ tests/unit/builders/test_dockerfile.py
@@ -33,3 +33,21 @@ ENV FAIRING_RUNTIME 1
 COPY /pre /pre
 RUN if [ -e requirements.txt ];then pip install --no-cache -r requirements.txt; fi"""
     assert actual == expected
+
+def test_writedockerfile_with_early_install_reqs():
+    _, tmp_file = tempfile.mkstemp()
+    dockerfile.write_dockerfile(
+                destination=tmp_file,
+                docker_command=["python", "main.py"],
+                path_prefix="/pre",
+                base_image="foo_bar",
+                install_reqs_before_copy=True)
+    actual = open(tmp_file, 'r').read()
+    expected = """FROM foo_bar
+WORKDIR /pre
+ENV FAIRING_RUNTIME 1
+COPY /pre/requirements.txt /pre
+RUN pip install --no-cache -r requirements.txt
+COPY /pre /pre
+CMD python main.py"""
+    assert actual == expected
