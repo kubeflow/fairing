bd8313d9a4e4dd3b69a840e7cf0ea0d2326d720e
Rajiv Pasricha <rpasricha@users.noreply.github.com>
2019-04-02 13:39:19 -0700

    Update full_notebook preprocessor to use Papermill (#162)
    * Update full_notebook preprocessor to use papermill
    
    * Add Papermill to requirements
    
    * Add tests for full_notebook preprocessor
    
    * Remove Papermill from requirements

--- fairing/preprocessors/full_notebook.py
+++ fairing/preprocessors/full_notebook.py
@@ -8,23 +8,34 @@ class FullNotebookPreProcessor(BasePreProcessor):
     # TODO: Allow configuration of errors / timeout options
     def __init__(self,
                  notebook_file=None,
+                 output_file="fairing_output_notebook.ipynb",
                  input_files=None,
-                 command=["jupyter", "nbconvert", "--stdout", "--to", "notebook", "--execute", "--allow-errors",
-                          "--ExecutePreprocessor.timeout=-1"],
+                 command=None,
                  path_prefix=constants.DEFAULT_DEST_PREFIX,
                  output_map=None):
 
         if notebook_file is None and notebook_util.is_in_notebook():
             notebook_file = notebook_util.get_notebook_name()
 
+        if notebook_file is None:
+            raise ValueError('A notebook_file must be provided.')
+
+        if command is None:
+            command = ["papermill", notebook_file, output_file, "--log-output"]
+
         input_files = input_files or []
         if notebook_file not in input_files:
             input_files.append(notebook_file)
 
         super().__init__(
-            executable=notebook_file,
+            executable=None,
             input_files=input_files,
             command=command,
             output_map=output_map,
             path_prefix=path_prefix)
+
+
+    # We don't want to set a default executable for the full_notebook preprocessor.
+    def set_default_executable(self):
+        pass
 
---
+++ tests/integration/common/test_full_notebook.py
@@ -0,0 +1,25 @@
+import pytest
+import fairing
+import os
+
+GCS_PROJECT_ID = fairing.cloud.gcp.guess_project_name()
+DOCKER_REGISTRY = 'gcr.io/{}'.format(GCS_PROJECT_ID)
+NOTEBOOK_PATH = os.path.join(os.path.dirname(__file__), 'test_notebook.ipynb')
+
+def run_full_notebook_submission(capsys, notebook_file, expected_result, deployer='job', builder='append'):
+    base_image = 'gcr.io/{}/fairing-test:latest'.format(GCS_PROJECT_ID)
+    fairing.config.set_builder(builder, base_image=base_image, registry=DOCKER_REGISTRY)
+    fairing.config.set_deployer(deployer, namespace='default')
+    fairing.config.set_preprocessor('full_notebook', notebook_file=notebook_file)
+
+    fairing.config.run()
+    captured = capsys.readouterr()
+    assert expected_result in captured.out
+
+def test_full_notebook_job(capsys):
+    run_full_notebook_submission(capsys, NOTEBOOK_PATH, 'Hello World',
+        deployer='job')
+
+def test_full_notebook_tfjob(capsys):
+    run_full_notebook_submission(capsys, NOTEBOOK_PATH, 'Hello World',
+        deployer='tfjob')
