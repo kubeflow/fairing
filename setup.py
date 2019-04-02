a3bc4967899a92c6caca5926a71abd0bfdae7c61
Karthik Ramasamy <karthikv2k@gmail.com>
2019-04-02 15:56:10 -0700

    Updating the package version to 0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='fairing',
-    version='0.0.3',
+    version='0.5',
     author="Kubeflow Authors",
     description="Python SDK for building, training, and deploying ML models",
     url="https://github.com/kubeflow/fairing",
