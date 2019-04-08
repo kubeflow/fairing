b01eddf77e0bbea4b505524a001c66219d5c3d3f
Matt Rickard <mrick@google.com>
2019-04-08 10:26:53 -0700

    add build version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='fairing',
-    version='0.5',
+    version='0.5.1',
     author="Kubeflow Authors",
     description="Python SDK for building, training, and deploying ML models",
     url="https://github.com/kubeflow/fairing",
