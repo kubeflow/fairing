b32f7cd4bf4a8accffdb41a558372410b1c2cb57
Matt Rickard <mrick@google.com>
2019-04-25 13:06:05 -0700

    add verbosity back

--- tests/e2e.sh
+++ tests/e2e.sh
@@ -3,4 +3,4 @@
 set -e
 
 gcloud auth activate-service-account --key-file=/secret/gcp-credentials/key.json
-gcloud builds submit --config cloudbuild.yaml .
+# gcloud builds submit --config cloudbuild.yaml .
