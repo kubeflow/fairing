eb86969a8e8bfc5e08dfc04d811d4b6fafbb8118
Chun-Hsiang Wang <chunhsiang@google.com>
2019-07-02 16:38:51 -0700

    add preprocessor to convert notebook (#229) * add filter to strip out everything except fairing:include-cell * add preprocessor to generate code from notebook and create an entrypoint

--- fairing/preprocessors/converted_notebook.py
+++ fairing/preprocessors/converted_notebook.py
@@ -25,6 +25,25 @@ class FilterMagicCommands(NbPreProcessor):
         return cell, resources
 
 
+class FilterIncludeCell(NbPreProcessor):
+    """Notebook preprocessor that only includes cells that have a comment fairing:include-cell"""
+    _pattern = re.compile('.*fairing:include-cell.*')
+
+    def filter_include_cell(self, src):
+        filtered = []
+        for line in src.splitlines():
+            match = self._pattern.match(line)
+            if match:
+                return src
+        return ''
+
+    def preprocess_cell(self, cell, resources, index):
+        if cell['cell_type'] == 'code':
+            cell['source'] = self.filter_include_cell(cell['source'])
+
+        return cell, resources
+
+
 class ConvertNotebookPreprocessor(BasePreProcessor):
     def __init__(self,
                  notebook_file=None,
@@ -55,3 +74,61 @@ class ConvertNotebookPreprocessor(BasePreProcessor):
             f.write(contents)
         self.executable = converted_notebook
         return [converted_notebook]
+
+
+class ConvertNotebookPreprocessorWithFire(ConvertNotebookPreprocessor):
+    """Create an entrpoint using pyfire."""
+    def __init__(self,
+                 class_name=None,
+                 notebook_file=None,
+                 notebook_preprocessor=FilterIncludeCell,
+                 executable=None,
+                 command=["python"],
+                 path_prefix=constants.DEFAULT_DEST_PREFIX,
+                 output_map=None):
+
+        super().__init__(
+            notebook_file=notebook_file,
+            notebook_preprocessor=notebook_preprocessor,
+            executable=executable,
+            command=command,
+            path_prefix=path_prefix,
+            output_map=output_map)
+
+        self.class_name = class_name
+
+    def preprocess(self):
+        exporter = nbconvert.PythonExporter()
+        exporter.register_preprocessor(self.notebook_preprocessor, enabled=True)
+        processed, _ = exporter.from_filename(self.notebook_file)
+
+        lines = []
+        for l in processed.splitlines():
+            # Get rid of multiple blank lines
+            if not l.strip():
+                if lines:
+                    if not lines[-1]:
+                        # last line is already blank don't add another one
+                        continue
+            # strip in statements
+            if l.startswith("# In["):
+                continue
+            lines.append(l)
+
+        contents = "\n".join(lines)
+        converted_notebook = Path(self.notebook_file).with_suffix('.py')
+        with open(converted_notebook, 'w') as f:
+            f.write(contents)
+            f.write("\n")
+            f.write("""
+if __name__ == "__main__":
+  import fire
+  import logging
+  logging.basicConfig(format='%(message)s')
+  logging.getLogger().setLevel(logging.INFO)
+  fire.Fire({0})
+""".format(self.class_name))
+        self.executable = converted_notebook
+        results =  [converted_notebook]
+        results.extend(self.input_files)
+        return results
