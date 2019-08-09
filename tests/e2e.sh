27ec96e950321d3c2fac6f5541f5957d095004b6
Jin Chi He <hejinchi@cn.ibm.com>
2019-08-09 08:20:35 +0800

    check the presummit test problem

--- tests/e2e.sh
+++ tests/e2e.sh
@@ -7,5 +7,8 @@ set -e
 
 pip3 install -U tornado
 
+sysctl fs.inotify.max_user_instances
+sysctl fs.inotify.max_user_instances=524288
+
 gcloud auth activate-service-account --key-file=/secret/gcp-credentials/key.json
 gcloud builds submit --config cloudbuild.yaml .
