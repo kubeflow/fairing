5325a19e7de81580fff746ee47b5dccf5aaf2437
Chun-Hsiang Wang <chunhsiang@google.com>
2019-08-26 02:16:21 -0700

    Fix bug in predict in examples. (#335)
    * Fix bug in predict in examples.
    
    * Install dependencies while running examples.
    
    * Fix.
    
    * Fix test integration error.
    
    * Fix typo.
    
    * Fix tornado error.
    
    * Rollback.
    
    * Fix.
    
    * Upgrade version of tornado.
    
    * Fix.
    
    * Fix tornado.
    
    * Fix pylint.
    
    * Making prediction output JSON serializable
    
    * adding missing arg in predict fn
    
    * reverting a change
    
    * enabling all tests

--- examples/prediction/HousingServe.py
+++ examples/prediction/HousingServe.py
@@ -30,13 +30,13 @@ class HousingServe(object):
         """Load the housing model using joblib."""
         self.model = joblib.load(model_file)
 
-    def predict(self, X, feature_names): #pylint:disable=unused-argument
+    def predict(self, X):
         """Predict using the model for given ndarray."""
         prediction = self.model.predict(data=X)
-        return [[prediction.item(0), prediction.item(0)]]
+        return [prediction]
 
 
 if __name__ == '__main__':
     fairing.config.run()
     serve = HousingServe()
-    print(serve.predict(np.ndarray([1, 37]), None))
+    print(serve.predict(np.ndarray([1, 37])))
