30492e65635ac56ce24098442ad96b90171d4e32
shikha <shikha130vv@gmail.com>
2019-09-04 10:15:09 +0530

    unit test docker

--- tests/unit/cloud/test_docker.py
+++ tests/unit/cloud/test_docker.py
@@ -1,9 +1,18 @@
-
 from fairing.cloud.docker import get_docker_secret_spec
 from fairing.constants import constants
+import json
+import os
 
 
 def test_docker_secret_spec():
+   os.environ["DOCKER_CONFIG"] = "/tmp"
+   config_dir = os.environ.get('DOCKER_CONFIG')
+   config_file_name = 'config.json'
+   config_file = os.path.join(config_dir, config_file_name)
+   with open(config_file, 'w+') as f:
+      json.dump({
+         'config': "config"
+      }, f)
    docker_secret=get_docker_secret_spec()
    print(docker_secret)
    assert docker_secret.metadata.name == constants.DOCKER_CREDS_SECRET_NAME
