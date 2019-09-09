68d1e2275bbd7ef0d145b7584490780574ed1b03
Jin Chi He <hejinchi@cn.ibm.com>
2019-09-10 01:01:20 +0800

    Update docstrings under kubernetes and functions (#346)

--- fairing/functions/function_shim.py
+++ fairing/functions/function_shim.py
@@ -14,6 +14,12 @@ class ObjectType(Enum):
     NOT_SUPPORTED = 3
 
 def get_execution_obj_type(obj):
+    """Get the execution object type, the object can be a function or class.
+
+    :param obj: The name of object such as the function or class
+    :returns: int: The corresponding object type.
+
+    """
     # Check if a function is provided
     if (isinstance(obj, types.FunctionType) or  #pylint:disable=consider-merging-isinstance
             isinstance(obj, types.BuiltinFunctionType) or
@@ -30,8 +36,14 @@ def get_execution_obj_type(obj):
 
     return ObjectType.NOT_SUPPORTED
 
-# compare the Python major and minor version for local and remote python
+
 def compare_version(local_python_version):
+    """Compare the Python major and minor version for local and remote python.
+
+    :param local_python_version: Python version of local environment
+    :returns: None.
+
+    """
     remote_python_version = ".".join([str(x) for x in sys.version_info[0:3]])
     if local_python_version != remote_python_version:
         raise RuntimeError('The Python version ' + remote_python_version + ' mismatches \
@@ -39,6 +51,12 @@ def compare_version(local_python_version):
 
 
 def call(serialized_fn_file):
+    """Get the content from serialized function.
+
+    :param serialized_fn_file: the file includes serialized function
+    :returns: object: The content of object.
+
+    """
     with open(serialized_fn_file, 'rb') as f:
         obj = cloudpickle.load(f)
         obj_type = get_execution_obj_type(obj)
--- fairing/utils.py
+++ fairing/utils.py
@@ -4,28 +4,46 @@ import uuid
 
 
 def get_image(repository, name):
+    """Get the full image name by integrating repository and image name.
+
+    :param repository: The name of repository
+    :param name: The short image name
+    :returns: str: Full image name, format: repo/name.
+
+    """
     return "{repo}/{name}".format(
         repo=repository,
         name=name
     )
 
 def is_running_in_k8s():
+    """Check if running in the kubernetes cluster."""
     return os.path.isdir('/var/run/secrets/kubernetes.io/')
 
 def get_current_k8s_namespace():
+    """Get the current namespace of kubernetes."""
     with open('/var/run/secrets/kubernetes.io/serviceaccount/namespace', 'r') as f:
         return f.readline()
 
 def get_default_target_namespace():
+    """Get the default target namespace, if running in the kubernetes cluster,
+    will be current namespace, Otherwiase, will be "default".
+    """
     if not is_running_in_k8s():
         return 'default'
     return get_current_k8s_namespace()
 
 def crc(file_name):
+    """Compute a running Cyclic Redundancy Check checksum.
+
+    :param file_name: The file name that's for crc checksum.
+
+    """
     prev = 0
     for eachLine in open(file_name, "rb"):
         prev = zlib.crc32(eachLine, prev)
     return "%X" % (prev & 0xFFFFFFFF)
 
 def random_tag():
+    """Get a random tag."""
     return str(uuid.uuid4()).split('-')[0]
