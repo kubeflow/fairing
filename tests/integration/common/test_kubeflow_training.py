10fdd04bfe075fe216f1874ae83d00625a0345a1
Abhishek Gupta <abhishek@google.com>
2019-10-16 10:46:10 -0700

    fixed all tests in common

--- tests/integration/common/test_kubeflow_training.py
+++ tests/integration/common/test_kubeflow_training.py
@@ -42,7 +42,7 @@ def run_submission_with_function_preprocessor(capsys, deployer="job", builder="a
     if builder == 'cluster':
         fairing.config.set_builder(builder, base_image=base_image, registry=DOCKER_REGISTRY,
                                    pod_spec_mutators=[
-                                       fairing.cloud.gcp.add_gcp_credentials],
+                                       fairing.cloud.gcp.add_gcp_credentials_if_exists],
                                    context_source=gcs_context.GCSContextSource(
                                        namespace=namespace),
                                    namespace=namespace)
