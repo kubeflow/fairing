ba602ce6787cedb6d74c05583392826f78aaef9a
Jin Chi He <hejinchi@cn.ibm.com>
2020-03-18 11:17:53 +0800

    Support IBM Cloud Backends

--- kubeflow/fairing/builders/cluster/cos_context.py
+++ kubeflow/fairing/builders/cluster/cos_context.py
@@ -1,29 +1,24 @@
+from kubernetes import client
 from kubeflow.fairing.builders.cluster.context_source import ContextSourceInterface
 from kubeflow.fairing.cloud import ibm_cloud
-from kubeflow.fairing.kubernetes.manager import client, KubeManager
 from kubeflow.fairing import utils
-from kubeflow.fairing import constants
-import ibm_boto3
-from ibm_botocore.client import Config, ClientError
+from kubeflow.fairing.constants import constants
 
 class COSContextSource(ContextSourceInterface):
-    def __init__(self, cos_endpoint_url, cos_auth_endpoint, cos_api_key,
-                 cos_resource_crn, cos_bucket_location=None):
+    """
+    IBM Cloud Object Storage Context Source.
+
+    :param namespace: namespace that IBM COS credential secret created in.
+    :param region: region name, default to us-geo
+    :param cos_endpoint_url: IBM COS endpoint url, such as "https://s3..."
+    """
+    def __init__(self, namespace=None, region='us-geo',
+                 cos_endpoint_url=constants.IBM_COS_DEFAULT_ENDPOINT):
         self.cos_endpoint_url = cos_endpoint_url
-        self.cos_auth_endpoint = cos_auth_endpoint
-        self.cos_api_key = cos_api_key
-        self.cos_resource_crn = cos_resource_crn
-        self.cos_bucket_location = cos_bucket_location
-        self.Manager = KubeManager()
-
-        cos_client = ibm_boto3.client("s3", 
-            ibm_api_key_id=self.cos_api_key,
-            ibm_service_instance_id=self.cos_resource_crn,
-            ibm_auth_endpoint=self.cos_auth_endpoint,
-            config=Config(signature_version="oauth"),
-            endpoint_url=self.cos_endpoint_url
-        )
-
+        self.region = region
+        self.namespace = namespace or utils.get_default_target_namespace()
+        self.aws_access_key_id, self.aws_secret_access_key =\
+            ibm_cloud.get_ibm_cos_credentials(namespace)
 
     def prepare(self, context_filename):  # pylint: disable=arguments-differ
         """
@@ -32,16 +27,16 @@ class COSContextSource(ContextSourceInterface):
         self.uploaded_context_url = self.upload_context(context_filename)
 
     def upload_context(self, context_filename):
+        """
+        :param context_filename: context filename
+        """
         cos_uploader = ibm_cloud.COSUploader(
-            self.cos_endpoint_url,
-            self.cos_auth_endpoint,
-            self.cos_api_key,
-            self.cos_resource_crn,
-            self.cos_bucket_location,
+            self.namespace,
+            self.cos_endpoint_url
         )
 
         context_hash = utils.crc(context_filename)
-        bucket_name = 'kubeflow-' + context_hash
+        bucket_name = 'kubeflow-' + context_hash.lower()
         return cos_uploader.upload_to_bucket(blob_name='fairing-builds/' +
                                              context_hash,
                                              bucket_name=bucket_name,
@@ -64,11 +59,30 @@ class COSContextSource(ContextSourceInterface):
             containers=[
                 client.V1Container(
                     name='kaniko',
-                    image=constants.constants.KANIKO_IMAGE,
+                    image=constants.KANIKO_IMAGE,
                     args=args,
+                    env=[
+                        client.V1EnvVar(name='AWS_REGION',
+                                        value=self.region),
+                        client.V1EnvVar(name='AWS_ACCESS_KEY_ID',
+                                        value=self.aws_access_key_id),
+                        client.V1EnvVar(name='AWS_SECRET_ACCESS_KEY',
+                                        value=self.aws_secret_access_key),
+                        client.V1EnvVar(name='S3_ENDPOINT',
+                                        value=self.cos_endpoint_url),
+                    ],
+                    volume_mounts=[
+                        client.V1VolumeMount(name="docker-config",
+                                             mount_path="/kaniko/.docker/")
+                    ]
                 )
             ],
-            restart_policy='Never')
+            restart_policy='Never',
+            volumes=[
+                client.V1Volume(name="docker-config",
+                                config_map=client.V1ConfigMapVolumeSource(
+                                    name="docker-config"))
+            ])
 
     def cleanup(self):
         # TODO(@jinchihe)
