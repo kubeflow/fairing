1142d68658597f18099625c486c0e4e8400d0c09
Ian Coffey <ian.coffey@gmail.com>
2020-05-14 04:56:22 -0400

    bump KANIKO_IMAGE to include important fixes (#493)
    * bump KANIKO_IMAGE to include important fixes
    
    * remove dupe constant definition in minio context

--- kubeflow/fairing/builders/cluster/minio_context.py
+++ kubeflow/fairing/builders/cluster/minio_context.py
@@ -4,8 +4,6 @@ from kubeflow.fairing.kubernetes.manager import client, KubeManager
 from kubeflow.fairing import utils
 from kubeflow.fairing import constants
 
-constants.constants.KANIKO_IMAGE = "gcr.io/kaniko-project/executor:v0.14.0"
-
 
 class MinioContextSource(ContextSourceInterface):
     def __init__(self, endpoint_url, minio_secret, minio_secret_key,
